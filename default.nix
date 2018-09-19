93caba20c7ba648326e637cd8b6f1b18166c6f65
Lionello Lunesu <lio+git@lunesu.com>
2018-09-19 16:33:47 +0800

    Added direnv and default.nix

---
+++ default.nix
@@ -0,0 +1,13 @@
+#with import (builtins.fetchTarball "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz") {};
+with (import <nixpkgs> {});
+
+mkShell {
+  buildInputs = [
+    nodejs-8_x nodePackages_8_x.pnpm
+  ];
+
+  shellHook = ''
+    export PATH=$PATH:$(pwd)/node_modules/.bin
+  '';
+}
+
